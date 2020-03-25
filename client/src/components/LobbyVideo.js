8a98a95900c0eaa637573c6b857a53947ebd59f3
Eric Kim <erickim195@gmail.com>
2020-03-25 11:57:20 -0700

    client - initial component setup

---
+++ client/src/components/LobbyVideo.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class LobbyVideo extends React.Component {
+    render = () => {
+        return (
+            <div className="lobby-video">
+                Lobby Video
+            </div>
+        )
+    }
+}
+
+export default LobbyVideo;
