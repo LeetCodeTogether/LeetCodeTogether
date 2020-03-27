12c7182aef66683b228f1c2df7c61e8905c66317
Jerry <hall.jerry9@gmail.com>
2020-03-27 01:22:48 -0700

    Added view

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import LobbyScreen from "./LobbyScreen.js";
+import View from "./View"
 
 class App extends React.Component {
     render = () => {
         return(
             <div className="app">
-                <LobbyScreen />
-                {/* TODO: Lobby Front Page*/}
+                <View />
             </div>
         );
     }
