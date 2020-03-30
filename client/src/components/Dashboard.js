799d1247df20e4a93663e42eaaa0604af1fcb15b
Jerry <hall.jerry9@gmail.com>
2020-03-29 22:42:13 -0700

    Implemented chart for num questions done

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import algorithms from '../test/algorithms.json';
+import Chart from './Chart.js'
 
 class Dashboard extends React.Component {
     render = () => {
+        const userData = JSON.parse(JSON.stringify(algorithms));
+        const displayUserData = Object.keys(userData).map((key) => {
+            return(
+                <div className="something">
+                    {key}: {JSON.stringify(userData[key])}
+                </div>
+            )
+        }) 
         return(
             <div className="Dashboard">
                 <h1>This is the Dashboard!</h1>
+                Below are my stats:
+                {/* {displayUserData} */}
+                < Chart />
             </div>
         )
     }
