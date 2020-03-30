433c79f2daf9a3babfc66f862e6915c96253b60a
Jerry <hall.jerry9@gmail.com>
2020-03-30 14:20:23 -0700

    Added snackbar when joining a lobby

--- client/src/components/Lobby.js
+++ client/src/components/Lobby.js
@@ -1,10 +1,21 @@
 import React from "react";
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert from '@material-ui/lab/Alert';
 
+function Alert(props) {
+    return <MuiAlert elevation={6} variant="filled" {...props} />;
+  }
+  
 class Lobby extends React.Component {
     render = () => {
-        return(
+        return (
             <div className="lobby">
                 <h1>This is a lobby!</h1>
+                <Snackbar open="true" autoHideDuration={6000}>
+                    <Alert severity="success">
+                     Joined &lt;Some Lobby Name>!
+                    </Alert>
+                </Snackbar>
             </div>
         );
     }
