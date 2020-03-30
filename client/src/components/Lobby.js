f2942ac0355ef6a807a4fb00a8737ee24cfe2461
Jerry <hall.jerry9@gmail.com>
2020-03-30 14:02:54 -0700

    Implemented lobbies page with single lobby

---
+++ client/src/components/Lobby.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Lobby extends React.Component {
+    render = () => {
+        return(
+            <div className="lobby">
+                <h1>This is a lobby!</h1>
+            </div>
+        );
+    }
+}
+
+export default Lobby;
