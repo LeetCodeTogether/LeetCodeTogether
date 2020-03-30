bbecb773cf309065e25e59c7ce75381ef7e8cdba
Eric Kim <erickim195@gmail.com>
2020-03-30 14:48:51 -0700

    backend - initialized with sockets

--- code_together/lib/code_together_web/channels/user_socket.ex
+++ code_together/lib/code_together_web/channels/user_socket.ex
@@ -2,7 +2,7 @@ defmodule CodeTogetherWeb.UserSocket do
   use Phoenix.Socket
 
   ## Channels
-  # channel "room:*", CodeTogetherWeb.RoomChannel
+  channel "room:*", CodeTogetherWeb.RoomChannel
 
   # Socket params are passed from the client and can
   # be used to verify and authenticate a user. After
