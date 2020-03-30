4b740b67b28931402fcf513ff1fa905ae2ba7e91
Jerry <hall.jerry9@gmail.com>
2020-03-30 16:46:16 -0700

    Styled Card in Lobby

--- client/src/components/Lobby.js
+++ client/src/components/Lobby.js
@@ -1,28 +1,46 @@
 import React from "react";
 import Snackbar from '@material-ui/core/Snackbar';
 import MuiAlert from '@material-ui/lab/Alert';
+import { Card } from "@material-ui/core";
+import CardContent from '@material-ui/core/CardContent';
+import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
 
 function Alert(props) {
     return <MuiAlert elevation={6} variant="filled" {...props} />;
-  }
-  
+}
+
+const styles = {
+    root: {
+        marginTop: "40px",
+        width: "80%",
+        height: "80%",
+        margin: "auto",
+    },
+}
+
 class Lobby extends React.Component {
     state = {
         open: true,
     }
-    handleClose = (event, reason) => {
+    handleClose = (reason) => {
         if (reason === 'clickaway') {
-          return;
+            return;
         }
-        this.setState({open: false})
+        this.setState({ open: false })
     };
-    render = () => {
+    render = (props) => {
+        const { classes } = this.props;
         return (
             <div className="lobby">
-                <h1>This is a lobby!</h1>
+                <Card className={classes.root}>
+                    <CardContent>
+                        This
+                    </CardContent>
+                </Card>
                 <Snackbar open={this.state.open} autoHideDuration={4000} onClose={this.handleClose}>
                     <Alert onClose={this.handleClose} severity="success">
-                     Joined &lt;Some Lobby Name>!
+                        Joined &lt;Some Lobby Name>!
                     </Alert>
                 </Snackbar>
             </div>
@@ -30,4 +48,4 @@ class Lobby extends React.Component {
     }
 }
 
-export default Lobby;
+export default withStyles(styles)(Lobby);
