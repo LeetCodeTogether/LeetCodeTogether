2168007434b6e7d169af4919d9029dc7d4b1c089
Jerry <hall.jerry9@gmail.com>
2020-03-31 00:37:34 -0700

    added ability to remove challenges

--- client/src/components/ChallengeView.js
+++ client/src/components/ChallengeView.js
@@ -3,23 +3,39 @@ import PopupState, { bindPopover, bindTrigger } from 'material-ui-popup-state';
 import React from 'react';
 import SendIcon from '@material-ui/icons/Send';
 
-const challenges = [
+// this should be the result of an api call
+const getChallenges = [
     { name: "Two sum", challenger: "Jerry", note: "I challenge you to do two sum in the next twenty minutes!", },
 ];
+
 export default function ChallengeView(props) {
+    const [challenges, setChallenges] = React.useState(getChallenges)
+    function doneChallenge(rem, i) {
+        hideChallenge(rem, i)
+        // add code to increment user score
+    };
+    function hideChallenge(rem, i) {
+        var arr = challenges.splice(i, i)
+        setChallenges(arr)
+    };
+
     return (
         <Paper style={props.styles.frame}>
             <Typography variant="button" style={{ paddingLeft: "8px" }}>Challenges</Typography>
             <Divider />
-            {challenges.map((challenge) => (
+            {challenges.map((challenge, index) => (
                 <Paper variant="outlined" style={props.styles.challenge}>
                     {challenge.name}: {challenge.note}
                     <Divider />
                     Challenger: {challenge.challenger}
                     <br />
                     <ButtonGroup variant="contained" color="primary" aria-label="contained primary button group" size="small">
-                        <Button style={{ backgroundColor: "#5cb85c", fontSize: "0.8em", }}>Did it</Button>
-                        <Button style={{ backgroundColor: "#d9534f", fontSize: "0.8em", }}>Hide</Button>
+                        <Button style={{ backgroundColor: "#5cb85c", fontSize: "0.8em", }} onClick={() => doneChallenge(challenge, index)}>
+                            Did it
+                        </Button>
+                        <Button style={{ backgroundColor: "#d9534f", fontSize: "0.8em", }} onClick={() => hideChallenge(challenge, index)}>
+                            Hide
+                        </Button>
                     </ButtonGroup>
                 </Paper>
             ))}
@@ -57,7 +73,7 @@ export default function ChallengeView(props) {
                                                 console.log(`Pressed keyCode ${ev.key}`);
                                                 ev.preventDefault();
                                             }
-                                        }} />    
+                                        }} />
                                     <Fab color="primary" aria-label="send" size="small" style={{ left: "4px", }}>
                                         <SendIcon />
                                     </Fab>
