b06cd5096ea0e8d4ad08977186f20dbc35aaf3f3
Jerry <hall.jerry9@gmail.com>
2020-04-05 00:27:15 -0700

    set up oauth via github

--- UseExpress/server.js
+++ UseExpress/server.js
@@ -1,13 +1,90 @@
+// Required dependencies 
 const express = require('express');
-const path = require('path');
 const app = express();
+const passport = require('passport');
+const GitHubStrategy = require('passport-github');
+const cookieSession = require('cookie-session');
+var findOrCreate = require('mongoose-findorcreate')
+const mongoose = require('mongoose');
 
-const PORT = process.env.PORT || 5000;
+//Configure Mongoose
+mongoose.connect('mongodb://localhost/code-together');
+mongoose.set('debug', true);
 
-app
-  .use(express.static(path.join(__dirname, '/client/build')))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+// Initialize users database
+const { Schema } = mongoose;
+const UsersSchema = new Schema({
+  githubId: String,
+}).plugin(findOrCreate);
+const Users = mongoose.model('Users', UsersSchema);
 
+// cookieSession config
+app.use(cookieSession({
+    maxAge: 24 * 60 * 60 * 1000, // One day in milliseconds
+    keys: ['jerryandericmadethebestappeverrrr']  // secret key to hash cookie ;)
+}));
+
+app.use(passport.initialize()); // Used to initialize passport
+app.use(passport.session()); // Used to persist login sessions
+
+// Strategy config
+passport.use(new GitHubStrategy({
+    clientID: 'Iv1.661622fe77dff4e6',
+    clientSecret: 'bf5e82daa699443f781424185a5db7ffcfb93fe9',
+    callbackURL: "http://localhost:5000/auth/github/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    Users.findOrCreate({ githubId: profile.id }, function (err, user) {
+      return done(err, user);
+    });
+  }
+));
+
+// Used to stuff a piece of information into a cookie
+passport.serializeUser((user, done) => {
+    done(null, user);
+});
+
+// Used to decode the received cookie and persist session
+passport.deserializeUser((user, done) => {
+    done(null, user);
+});
+
+// Middleware to check if the user is authenticated
+function isUserAuthenticated(req, res, next) {
+    if (req.user) {
+        next();
+    } else {
+        res.send('You must login!');
+    }
+}
+
+// Routes
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/client/build/public/index.html'));
+    res.render('index.ejs');
+});
+
+// passport.authenticate middleware is used here to authenticate the request
+app.get('/auth/github', passport.authenticate('github', {
+    scope: ['(no scope)'] // Used to specify the required data; we only want read-only access to public information
+}));
+
+// The middleware receives the data from Google and runs the function on Strategy config
+app.get('/auth/github/callback', passport.authenticate('github'), (req, res) => {
+    res.redirect('/');
+});
+
+// Secret route
+app.get('/secret', isUserAuthenticated, (req, res) => {
+    res.send('You have reached the secret route');
+});
+
+// Logout route
+app.get('/logout', (req, res) => {
+    req.logout(); 
+    res.redirect('/');
+});
+
+app.listen(5000, () => {
+    console.log('Server Started!');
 });
