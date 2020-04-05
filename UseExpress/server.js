110a7259b1e725e122971564b47c7f95be683879
Jerry <hall.jerry9@gmail.com>
2020-04-04 22:17:03 -0700

    wip

--- UseExpress/server.js
+++ UseExpress/server.js
@@ -8,7 +8,7 @@ app.use(express.static(path.join(__dirname, 'client/build')));
 
 // Handles any requests that don't match the ones above
 app.get('*', (req,res) =>{
-    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+    res.sendFile(path.join(__dirname+'/client/build/public/index.html'));
 });
 
 const port = process.env.PORT || 5000;
